81924dea0404ebd4084396111736396a6c699ffc
Kenneth Chung <kennethchung@hotmail.com>
2014-04-27 19:41:57 -0700

    R Programming Homework 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function converts a R based matrix into a cached matrix and
+## and store inverse of the result
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(solve) m <<- solve
+	getInverse <- function() m
+	list(set = set, get = get,
+			setInverse = setInverse,
+			getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function returns the inverse of a matrix from the cached result if 
+## it was calculated before otherwise it will calculcate the inverse and store the result
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getInverse()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setInverse(m)
+	m
 }
+
+
